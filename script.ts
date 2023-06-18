df3c883fdaff476fc6770c4714b90c02644da5a4
MrTESSARD <dimitri@tessard.pro>
2023-06-18 22:16:53 +0200

    51 - Utility Type - Extract

--- script.ts
+++ script.ts
@@ -1,41 +1,12 @@
-// 50 - Utility Type - Exclude
+// 51 - Utility Type - Extract
 
-// type T0=Exclude<"a"| "b"| "c"| (()=>void), Function>
-type Easing="ease_in"| "ease_out"| "ease_in_out"
-type Animate=Exclude<Easing,"ease_in">
-
-// class UIElement {
-//   animate(x:number,y:number, easing:Easing){
-//     if (easing==="ease_in") {
-//       console.log("ease_in");
-      
-//     }
-//     if (easing==="ease_out") {
-//       console.log("ease_out");
-      
-//     }
-//     if (easing==="ease_in_out") {
-//       console.log("ease_in_out");
-      
-//     }
-//   }
-// }
-class UIElement {
-  animate(x:number,y:number, easing:Animate){
-    // if (easing==="ease_in") {//Exclude
-    //   console.log("ease_in");
-      
-    // }
-    if (easing==="ease_out") {
-      console.log("ease_out");
-      
-    }
-    if (easing==="ease_in_out") {
-      console.log("ease_in_out");
-      
-    }
-  }
+type Colors="red"| "blue"| "orange"|"green"
+type MyColors="red"| "purpre"| "green"
+type AcceptedColors=Extract<Colors,MyColors>
+function clgColors(arg:AcceptedColors) {
+  console.log(arg);
+  
 }
-
-let button=new UIElement()
-button.animate(0,0,"ease_in")
+clgColors("red")
+clgColors("green")
+// clgColors("blue")//nok
