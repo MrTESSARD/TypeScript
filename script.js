a9ff66096578e52e3f205a4adebf905d3f06657f
MrTESSARD <dimitri@tessard.pro>
2023-06-18 21:44:49 +0200

    48 - Utility Types - Pick

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
-// 47. Utility Types - Record
-var heros = {
-    Barman: { name: "Batman", power: 60, marvel: false, dcComics: true },
-    Spiderman: { name: "Spiderman", power: 60, marvel: true, dcComics: false },
-    Superman: { name: "Superman", power: 60, marvel: false, dcComics: true } // Enregistrement pour le super-héros "Superman"
+// 48 - Utility Types - Pick
+var todo1 = {
+    title: "Sport",
+    description: "Footing",
+    completed: true
 };
-console.log(heros); // Affiche le contenu de la variable heros dans la console
-heros.Spiderman.marvel && console.log("".concat(heros.Spiderman.name, " est Marvel")); // Condition pour vérifier si Spiderman appartient à Marvel, et affiche un message correspondant dans la console si c'est le cas
+var todo2 = {
+    title: "fare les courses",
+    completed: false
+};
