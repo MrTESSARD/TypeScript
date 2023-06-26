4d881857f1fc1c146f61ed4fa67e805c13f60978
MrTESSARD <dimitri@tessard.pro>
2023-06-26 13:43:57 +0200

    79. Decorators Méthodes - Accessors 1/3

--- src/script.ts
+++ src/script.ts
@@ -1,23 +1,48 @@
-// 78. Decorator pour une propriété de classe
-function PropretyLog(target:any, propretykey:string) {
-  console.log(target);
-  console.log(propretykey);
+// 79. Decorators Méthodes - Accessors
+
+function setterLog(
+  target: any, propertyKey: string, descriptor: PropertyDescriptor
+  ) {
+    // console.log(target); //prototype de la class
+    // console.log(propertyKey);//setColor
+    // console.log(descriptor);//propriétés du descriptor
   
 }
-
+function infosLog(
+  target: any, propertyKey: string, descriptor: PropertyDescriptor
+  ) {
+    console.log(target); //prototype de la class
+    console.log(propertyKey);//setColor
+    console.log(descriptor);//propriétés du descriptor
+  
+}
 class Car {
-  @PropretyLog
   brand:string
-  color:string
+  private color:string
 
-   constructor(b:string,c:string) {
+  constructor(b:string, c:string) {
     this.brand=b
     this.color=c
+
+    
   }
-  getInfos(){
-    return `Marque : ${this.brand} | Couleur : ${this.color}`
+  @setterLog
+  setColor(color:string){//private
+    this.color=color
+  }
+  @infosLog
+  infos(){//public
+    return`Marque : ${this.brand} | ${this.color}`
   }
 }
 
-const opel = new Car("Opel", "red")
-console.log(opel);
+const opel=new Car("Opel","red")
+// console.log(opel);
+console.log(opel.infos());
+opel.infos=function () {
+  return "Hello"
+  
+}
+console.log(opel.infos());
+
+
